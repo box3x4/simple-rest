4a267c0ebb63a8adfbd3973924954d1e8fa4d139
Bruno Barreto Carvalho <bruno.carvalho1505@hotmail.com>
2017-08-01 11:23:13 -0300

    adding async await

--- routes/api.js
+++ routes/api.js
@@ -1,43 +1,46 @@
 let Book = require('../model/books.js');
 
-const get = (req, res, next) => {
+async function get(req, res, next) {
 
-    Book.find(req.query, (err, docs) => {
-        
-        if(err) {      
-            return res.status(500).send(err);
-        }
+    let docs;
 
-        if(docs.length) {
-            return res.status(200).send(docs);
-        } else {
-            return res.status(204).send([]);
-        }
-    });
+    try {
+        docs = await Book.find(req.query);
+    } catch(err) {
+        return res.status(500).send(err);
+    }
+
+    if(docs.length)
+        return res.status(200).send(docs);
+    else
+        return res.status(204).send([]);
+
 };
 
-const post = (req, res, next) => {
+async function post(req, res, next) {
 
-    Book.findOneAndUpdate({book_name: req.body.book_name}, req.body, { upsert: true }, (err, doc) => {
+    let doc;
+
+    try {
+        doc = await Book.findOneAndUpdate({book_name: req.body.book_name}, req.body, { upsert: true });
+    } catch(err) {
+        return res.status(500).send(err);
+    }
         
-        if(err) {
-            return res.status(500).send(err);
-        }
-        
-        return res.status(201).send(doc);
-    });
+    return res.status(201).send();
 };
 
-const del = (req, res, next) => {
+async function del(req, res, next) {
 
-    Book.findOneAndRemove({book_name: req.query.book_name}, (err, resp) => {
+    let resp;
 
-        if(err) {
-            return res.status(500).send(err);
-        }
+    try {
+        resp = await Book.findOneAndRemove({book_name: req.query.book_name});
+    } catch(err) {
+        return res.status(500).send(err);
+    }
 
-        return res.status(200).send(resp);
-    });
+    return res.status(200).send(resp);
 };
 
 module.exports = {
